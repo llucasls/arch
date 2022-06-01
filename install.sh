c6aaa0f52c054a28204b8c6e95f80b619a26cf3e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-01 01:36:52 -0300

    add safecheck for current distribution

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+eval " $(cat /etc/os-release)"
+if [ "$NAME" != "Arch Linux" ]; then
+    echo "Error: current distribution is not Arch Linux" > /dev/stderr
+fi
+
 timedatectl set-ntp true

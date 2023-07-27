ed6940eee339a2e5e5cf5b57e55aac1324666594
Lucas. L. S. Haine <lucaslshaine@gmail.com>
2023-07-27 16:12:03 -0300

    clone repositories

--- post_install.sh
+++ post_install.sh
@@ -4,3 +4,23 @@ PACKAGES=$(< arch_packages)
 FONTS=$(< arch_fonts)
 
 pacman -S ${PACKAGES} ${FONTS}
+
+USER=lucas
+HOME=/home/${USER}
+REPOS_DIR=${HOME}/.repos
+
+mkdir -p ${REPOS_DIR}
+
+git clone https://github.com/llucasls/dwm.git ${REPOS_DIR}/dwm
+make --file=${REPOS_DIR}/dwm/Makefile install clean
+
+git clone https://github.com/llucasls/dmenu.git ${REPOS_DIR}/dmenu
+make --file=${REPOS_DIR}/dmenu/Makefile install clean
+
+git clone https://github.com/llucasls/st.git ${REPOS_DIR}/st
+make --file=${REPOS_DIR}/st/Makefile install clean
+
+git clone https://github.com/llucasls/tabbed.git ${REPOS_DIR}/tabbed
+make --file=${REPOS_DIR}/tabbed/Makefile install clean
+
+chown -R ${USER}:${USER} ${HOME}
